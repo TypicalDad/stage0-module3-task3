fe8270be463c908f9e312216faeb99d73e84fd53
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-05 12:32:16 +0300

    Update BasicVariablesInitialization.java

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -8,6 +8,6 @@ public class BasicVariablesInitialization {
         first = 1;
         second = 10;
         third = 100;
-        System.out.println(first +"\n" +second+"\n" +third);
+        System.out.println(first +"\n" +second+"\n" +third+"\n");
     }
 }
